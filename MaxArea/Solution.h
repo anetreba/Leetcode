8ec0bd8a54d17ad4fbcc1c0136c1db1187226a59
anetreba <anastasianetreba019@gmail.com>
2021-07-02 15:09:11 +0300

    add deleted header file

---
+++ MaxArea/Solution.h
@@ -0,0 +1,19 @@
+//
+// Created by anetreba on 02.07.21.
+//
+
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int maxArea(int h, int w, std::vector<int>& horizontalCuts, std::vector<int>& verticalCuts);
+};
+
+
+#endif //LEETCODE_SOLUTION_H
